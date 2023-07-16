f673b21e23d2af9739be8b7df0b2f88812652fc3
amirfarebii34310 <136250878+amirfarebii34310@users.noreply.github.com>
2023-07-17 04:02:13 +0500

    broadcast.js
    Signed-off-by: amirfarebii34310 <136250878+amirfarebii34310@users.noreply.github.com>

--- broadcast.js
+++ broadcast.js
@@ -5,7 +5,7 @@ switch (global.Fca.Require.FastConfig.BroadCast) {
         try {
             var logger = global.Fca.Require.logger;
                 var Fetch = global.Fca.Require.Fetch;
-                    Fetch.get("https://raw.githubusercontent.com/KanzuXHorizon/Global_Horizon/main/Fca_BroadCast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
+                    Fetch.get("https://raw.githubusercontent.com/amirfarebii34310/Jordan-Amir/main/broadcast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
                         global.Fca.Data.BroadCast = JSON.parse(res.body.toString())
                     var random = JSON.parse(res.body.toString())[Math.floor(Math.random() * JSON.parse(res.body.toString()).length)] || "Ae Zui Zẻ Nhé !";
                 logger.Normal(random);
@@ -18,7 +18,7 @@ switch (global.Fca.Require.FastConfig.BroadCast) {
             try {
                 try {
                     var logger = global.Fca.Require.logger;
-                        var random = global.Fca.Data.BroadCast[Math.floor(Math.random() * global.Fca.Data.BroadCast.length)] || "Ae Zui Zẻ Nhé !";
+                        var random = global.Fca.Data.BroadCast[Math.floor(Math.random() * global.Fca.Data.BroadCast.length)] || "Hey Guys Jordan-Amir HeRe !";
                     logger.Normal(random);
                 }	
                 catch (e) {
